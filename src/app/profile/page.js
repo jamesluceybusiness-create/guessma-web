04069f02762fcb95ae33efcb7462310f246cd817
jlucey55 <luceyjames2021@gmail.com>
2026-06-21 17:20:12 -0700

    remove duplicate profile page

--- src/app/profile/page.js
+++
@@ -1,7 +0,0 @@
-export default function ProfilePage() {
-  return (
-    <div style={{ minHeight: '100vh', background: '#0a0a0f', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-      <p style={{ color: 'white', fontSize: '1rem', fontWeight: 600 }}>Profile coming soon</p>
-    </div>
-  )
-}
